aebd5331633eb014f7e8c21e396aaa6306a101b0
Dani <daniel.blara@gmail.com>
2015-09-28 17:15:25 +0200

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('dablarStockmarketAppApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
